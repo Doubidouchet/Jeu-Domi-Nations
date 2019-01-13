49dce4736c2ad379608b6c253bcc5eea7b28790b
bby360 <45206925+bby360@users.noreply.github.com>
2019-01-13 23:56:51 +0100

    Add files via upload

--- DOMINATION/src/jeu/JoueurTour.java
+++ DOMINATION/src/jeu/JoueurTour.java
@@ -16,24 +16,7 @@ public class JoueurTour {
 		return rsvPlateau;
 	}
 	
-	public void setRsvPlateau(){
-		// on creer la reserve
-		for (int i = 0; i < rsvPlateau.length; i++){
-			rsvPlateau[i] = donneRandomDomino();
-		}
-		 for (int i = 0; i < rsvPlateau.length - 1; i++)
-	        {
-	            int index = i;
-	            for (int j = i + 1; j < rsvPlateau.length; j++)
-	                if (rsvPlateau[j].getNombre() < rsvPlateau[index].getNombre()) 
-	                    index = j;
-	      
-	            Tuiles petitDomino = rsvPlateau[index];  
-	            rsvPlateau[index] = rsvPlateau[i];
-	            rsvPlateau[i] = petitDomino;
-	        }
-		
-	}
+
 	
 	// Donne un domino aleatoire dans le tour
 	
@@ -51,5 +34,24 @@ public class JoueurTour {
 			}
 		}
 		return leTour;
+	}
+	
+	public void setRsvPlateau(){
+		// on creer la reserve
+		for (int i = 0; i < rsvPlateau.length; i++){
+			rsvPlateau[i] = donneRandomDomino();
+		}
+		 for (int i = 0; i < rsvPlateau.length - 1; i++)
+	        {
+	           int index = i;
+	           for (int j = i + 1; j < rsvPlateau.length; j++)
+	                if (rsvPlateau[j].getNombre() < rsvPlateau[index].getNombre()) 
+	                    index = j;
+	      
+	            Tuiles petitDomino = rsvPlateau[index];  
+	            rsvPlateau[index] = rsvPlateau[i];
+	            rsvPlateau[i] = petitDomino;
+	        }
+		
 	}
 }
